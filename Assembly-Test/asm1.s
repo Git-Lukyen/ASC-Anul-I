1de39c8e83fe9e9a37e71747080b24391dc9a4aa
lucian <dracea.lucian.mihai@gmail.com>
2023-10-16 22:14:45 +0300

    initial commit

---
+++ Assembly-Test/asm1.s
@@ -0,0 +1,21 @@
+.data
+#zona de date
+x: .long 15
+y: .byte 65
+z: .asciz "Hello world!\n"
+
+.text
+.global main
+main:
+#cod
+
+mov $4, %eax
+mov $1, %ebx #in - 0, out - 1
+mov $x, %ecx
+mov $5, %edx
+int $0x80
+
+#return 0
+mov $1, %eax
+mov $0, %ebx
+int $0x80
